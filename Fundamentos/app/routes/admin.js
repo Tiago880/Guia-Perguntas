f19146eaaadec6da93d6a95bbeaae107b83ba05c
Tiago <tiagocruz_880@hotmail.com>
2021-12-04 11:07:56 -0300

    conceitos - 02

--- Fundamentos/app/routes/admin.js
+++ Fundamentos/app/routes/admin.js
@@ -9,7 +9,22 @@ application.post('/noticias/salvar', (request, response) => {
     
     const noticia = request.body;
 
+    console.log(noticia);
 
+        request.assert('titulo','Titulo é obrigatório').notEmpty();
+        request.assert('resumo','Resumo é obrigatório').notEmpty();
+        request.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10,100);
+        request.assert('autor','Autor é obrigatório').notEmpty();
+        request.assert('data_noticia','Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        request.assert('noticia','Noticia é obrigatório').notEmpty();
+
+
+      var erros = request.validationErrors();
+
+        if(erros){
+            response.render("admin/formulario");
+            return;
+        }
         const connection = application.config.dbConnection();
         const noticiasModel = new application.app.models.NoticiasDAO(connection);
         
