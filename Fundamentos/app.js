b31e2b506d82aa503fffad816b4c13eddfdb9456
Tiago880 <tiagocruz_880@hotmail.com>
2022-05-14 13:02:44 -0300

    refresh

--- Fundamentos/app.js
+++
@@ -1,5 +0,0 @@
-const app = require('./config/server');
-
-app.listen(3000, function(){
-    console.log('Servidor ON');
-});
--- Fundamentos/app/routes/home.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app){
-
-app.get('/', (request, response) => {
-    response.render("home/index");
-});
-
-};
